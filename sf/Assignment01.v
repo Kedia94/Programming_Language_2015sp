485397242f55099606235294dff255f43aa5062d
kedia94 <kedia94@snu.ac.kr>
2015-03-19 13:13:11 +0900

    Hw01 Submitted

--- sf/Assignment01.v
+++ sf/Assignment01.v
@@ -5,34 +5,38 @@
 
 Definition admit {T: Type} : T.  Admitted.
 
-(** **** Problem #1 : 1 star (andb3) *)
+(** **** Exercise: 1 star (andb3) *)
 (** Do the same for the [andb3] function below. This function should
     return [true] when all of its inputs are [true], and [false]
     otherwise. *)
 
 Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
-  (* FILL IN HERE *) admit.
-
+    match b1, b2 with
+    | true, true => b3
+    | _, _ => false
+    end.
 Example test_andb31:                 (andb3 true true true) = true.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
+
 Example test_andb32:                 (andb3 false true true) = false.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
+
 Example test_andb33:                 (andb3 true false true) = false.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
+
 Example test_andb34:                 (andb3 true true false) = false.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
 (** [] *)
 
 
 
-(** **** Problem #2: 1 star (factorial) *)
+(** **** Exercise: 1 star (factorial) *)
 (** Recall the standard factorial function:
 <<
     factorial(0)  =  1 
     factorial(n)  =  n * factorial(n-1)     (if n>0)
 >>
     Translate this into Coq. 
-
     Note that plus and multiplication are already defined in Coq.
     use "+" for plus and "*" for multiplication.
 *)
@@ -41,27 +45,43 @@ Eval compute in 3 * 5.
 Eval compute in 3+5*6.
 
 Fixpoint factorial (n:nat) : nat := 
-(* FILL IN HERE *) admit.
+    match n with
+    | 0 => 1
+    | S n' => n * factorial n'
+end.
 
 Example test_factorial1:          (factorial 3) = 6.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
+ 
 Example test_factorial2:          (factorial 5) = 10 * 12.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
 (** [] *)
 
 
 
-(** **** Problem #3: 2 stars (blt_nat) *)
+(** **** Exercise: 2 stars (blt_nat) *)
 (** The [blt_nat] function tests [nat]ural numbers for [l]ess-[t]han,
-    yielding a [b]oolean.  Use [Fixpoint] to define it. *)
-
-Definition blt_nat (n m : nat) : bool :=
-  (* FILL IN HERE *) admit.
+    yielding a [b]oolean.  Instead of making up a new [Fixpoint] for
+    this one, define it in terms of a previously defined function.  
+    
+    Note: If you have trouble with the [simpl] tactic, try using
+    [compute], which is like [simpl] on steroids.  However, there is a
+    simple, elegant solution for which [simpl] suffices. *)
+Eval compute in Prop.
+Definition blt_nat :=
+    fix recur_blt_nat(n m : nat) : bool :=
+        match n, m with
+        | 0, S m' => true
+        | _, 0 => false
+        | S n', S m' => recur_blt_nat n' m'
+        end.
 
 Example test_blt_nat1:             (blt_nat 2 2) = false.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
+
 Example test_blt_nat2:             (blt_nat 2 4) = true.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
+
 Example test_blt_nat3:             (blt_nat 4 2) = false.
-(* FILL IN HERE *) Admitted.
+simpl. reflexivity. Qed.
 (** [] *)
