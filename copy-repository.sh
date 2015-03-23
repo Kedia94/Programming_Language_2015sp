59829fd8f4fff3a9abe279415fc388855f289f23
kedia94 <kedia94@snu.ac.kr>
2015-03-23 23:04:48 +0900

    TEST

--- copy-repository.sh
+++ copy-repository.sh
@@ -5,13 +5,13 @@
 ## Configurations ##
 
 # You have to write down your own GitHub user id.
-if [ -z $USERID ] ; then
+if [ -z kedia94 ] ; then
   echo "You have to specify USERID."
   exit 1
 fi
 
 # You have to write down the directory you want to place the repository.
-if [ -z $WORKSPACE ] ; then
+if [ -z pl2015 ] ; then
   echo "You have to specify WORKSPACE."
   exit 1
 fi
@@ -19,7 +19,7 @@ fi
 ## Execution ##
 
 # Go to the workspace.
-cd $WORKSPACE
+
 
 # Download the original repository.
 git clone https://github.com/snu-sf/pl2015.git
@@ -27,6 +27,6 @@ git clone https://github.com/snu-sf/pl2015.git
 # Copy.
 cd pl2015
 git remote rm origin
-git remote add origin https://github.com/$USERID/pl2015.git
+git remote add origin https://github.com/kedia94/pl2015.git
 git remote add upstream https://github.com/snu-sf/pl2015.git
 git push --set-upstream origin master
