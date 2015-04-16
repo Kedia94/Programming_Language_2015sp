cfe1ace7b340eac91fe03d09eba2ab2aa5326021
kedia94 <kedia94@snu.ac.kr>
2015-04-16 20:19:07 +0900

    HW5 submitted.

--- sf/Assignment05_27.v
+++ sf/Assignment05_27.v
@@ -1,21 +1,11 @@
 Require Export Assignment05_26.
 
 (* problem #27: 10 points *)
-Definition nat_ind2: forall (P: nat -> Prop), 
-           P 0 -> 
-           P 1 -> 
-          (forall n: nat, P n -> P (S(S n))) -> 
-          forall n: nat, P n := 
-                     fun P => fun P0 => fun P1 => fun PSS => 
-                     fix f (n: nat) := match n return P n with 0 => P0 | 1 => P1 | S (S n') => PSS n' (f n')
-end.
-
 
 Theorem even__ev: forall n : nat,
   even n -> ev n.
 Proof.
-intro n. unfold even. intro H.
-induction n using nat_ind2. apply ev_0. inversion H. apply ev_SS. apply IHn. simpl in H. apply H.
+apply even__ev_strong.
   (* FILL IN HERE *)
 Qed.
 (** [] *)
